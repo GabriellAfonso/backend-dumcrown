4c62bfe73e7778373ba85d33bffde3855f54a449
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-10-29 14:53:36 -0300

    add PlayerSettings model for user preferences and configurations

---
+++ server/dumcrown/models/settings.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class PlayerSettings(models.Model):
+    profile = models.OneToOneField(
+        'PlayerProfile', on_delete=models.CASCADE, related_name='settings')
+    master_volume = models.IntegerField(default=100)
+    music_volume = models.IntegerField(default=80)
+    sfx_volume = models.IntegerField(default=80)
+    resolution_width = models.IntegerField(default=1920)
+    resolution_height = models.IntegerField(default=1080)
+    notifications_enabled = models.BooleanField(default=True)
+    language_preference = models.CharField(max_length=10, default='en')
