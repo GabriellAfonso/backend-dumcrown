ec648fac3fdc629c2056819f6c41453248eb2acf
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-10-29 17:00:29 -0300

    register admin models for PlayerProfile, PlayerStats, LoginHistory, and PlayerSettings; add user registration and login example requests

--- server/dumcrown/admin.py
+++ server/dumcrown/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from dumcrown.models.player import PlayerProfile, PlayerStats, LoginHistory
+from dumcrown.models.settings import PlayerSettings
+
 
 # Register your models here.
+
+
+@admin.register(PlayerProfile)
+class PlayerProfileAdmin(admin.ModelAdmin):
+    ...
+    list_display = ('nickname', 'user', 'level',
+                    'experience_points', 'coins', 'credits')
+
+
+@admin.register(PlayerStats)
+class PlayerStatsAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(LoginHistory)
+class LoginHistoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(PlayerSettings)
+class PlayerSettingsAdmin(admin.ModelAdmin):
+    ...
