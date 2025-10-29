a87f01924f6422de6554f36a4b81c6db1ea1159c
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-10-29 16:58:43 -0300

    implement user registration and login views with JWT authentication

--- server/dumcrown/urls.py
+++ server/dumcrown/urls.py
@@ -1,9 +1,9 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from dumcrown.views import teste
-
+from django.urls import path
+from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', teste,),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- server/dumcrown/views.py
+++ server/dumcrown/views.py
@@ -1,5 +1,41 @@
-from django.http import HttpResponse
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
+from dumcrown.models.player import PlayerProfile
+from django.utils import timezone
 
 
-def teste(request):
-    return HttpResponse("ola mundo")
+class RegisterView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"detail": "User registered successfully"}, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # Atualiza last_login manualmente
+            user.last_login = timezone.now()
+            user.save(update_fields=["last_login"])
+
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token)
+            })
+        else:
+            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
