5181518249a878b489a517db515a4c1372093e0a
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-12-01 14:55:32 -0300

    feat: update .gitignore, add requirements.txt, and create commands.sh for deployment setup

---
+++ scripts/commands.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+#   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+#   sleep 2
+# done
+
+# echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+sleep 0.2
+
+# Iniciar Daphne
+daphne -b 0.0.0.0 -p 8000 core.asgi:application
+
