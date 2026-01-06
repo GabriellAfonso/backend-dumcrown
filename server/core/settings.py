e6c46ddd993b38ed903c656114abf93be9435d57
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-06 14:17:22 -0300

    refactor: update Docker Compose configuration for anathema server and redis

--- server/core/settings.py
+++ server/core/settings.py
@@ -73,7 +73,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("dum_redis", 6379)],
+            "hosts": [("anathema_redis", 6379)],
         },
     },
 }
