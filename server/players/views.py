e8100130c8eb89b512955a725a5ee45ce3bb9881
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-06 21:46:40 -0300

    feat: implement PlayerMeView to retrieve authenticated user's profile

--- server/players/views.py
+++ server/players/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
 
-# Create your views here.
+from players.serializers import PlayerSerializer
+
+
+class PlayerMeView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request: Request) -> Response:
+        player = request.user.profile
+        serializer = PlayerSerializer(player)
+        return Response(serializer.data)
